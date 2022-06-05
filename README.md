393f14f41364df481261db1bf25cd6e456cf39a6
ilovesshan <ilovesshany@gmail.com>
2022-06-05 12:26:54 +0800

    mp-uploader 图片上传

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ##### 1、使用Promise对wx.request()进行二次封装。
 ##### 2、实现一个自定义数据版省市区二级、三级联动。([参考并感谢](https://developers.weixin.qq.com/community/develop/article/doc/0000643f674fa81a18a92b37455413))
 ##### 3、自定义Tabbar。
+##### 4、Picker组件(WEUI)和Picker组件(native)。
--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -1,30 +1,48 @@
 // pages/profile/profile.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    files: []
+},
 
+  onLoad() {
+    this.setData({
+      selectFile: this.selectFile.bind(this),
+      uplaodFile: this.uplaodFile.bind(this)
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
+  selectFile(files) {
+      console.log('files', files)
+      // 返回false可以阻止某次文件上传
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
+  uplaodFile(files) {
+    console.log('upload files', files)
+    // 文件上传的函数，返回一个promise
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        console.log(files["tempFilePaths"][0]);
+          //reject('some error')
 
+          const uploadImagePath = [];
+
+          files["tempFilePaths"].forEach(item =>{
+            uploadImagePath.push(item);
+          })
+
+          resolve({urls:uploadImagePath})
+      }, 2000)
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
+  uploadError(e) {
+    console.log('upload error', e.detail)
+  },
+
+  uploadSuccess(e) {
+    console.log('upload success', e.detail)
+  },
+
   onShow() {
     if (typeof this.getTabBar === 'function' && this.getTabBar()) {
       this.getTabBar().setData({
@@ -32,39 +50,4 @@ Page({
       });
     }
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
 })
